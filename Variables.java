eab5364998386e81205c2b6a227b3ccd22ee24bb
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-07 22:10:19 +0530

    Learning about Variables

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String args[]) {
+        int a=10;
+        int b=5;
+        String name="Aniket Agarwal";
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        a=30;
+        System.out.println(a);
+        a=b;
+        System.out.println(a);
+    }
+}
