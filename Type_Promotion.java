1ebedcb3725de6d075cddea84422529a205d0163
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-08 13:14:05 +0530

    Converting small data type into large data type during expression

---
+++ Type_Promotion.java
@@ -0,0 +1,9 @@
+  public class Type_Promotion {
+    public static void main(String args[]) {
+        char a='a';
+        short b=30;
+        System.out.println(a+b);
+        char c='b';
+        System.out.println(a-c);
+    }
+}
