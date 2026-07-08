b0d28a494f2bee80b6493516aa9bac61e8ce5d4e
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-08 13:10:29 +0530

    Changing the type of Variables

---
+++ Type_casting.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Type_casting {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        float a=25.29f;
+        int b=(int)a;
+        System.out.println(b);
+    }
+}
