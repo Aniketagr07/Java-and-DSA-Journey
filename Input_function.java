74022011c2b423e0eb4e99943a33d725673762be
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-08 13:02:40 +0530

    Learning how to take input in Java

---
+++ Input_function.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Input_function {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        /*
+        String input=sc.next();
+        System.out.println(input);
+        */
+        //String name=sc.nextLine();
+        //System.out.println(name);
+        /*
+        int number=sc.nextInt();
+        System.out.println(number);
+
+        float price=sc.nextFloat();
+        System.out.println(price);
+        */
+        boolean b=sc.nextBoolean();
+        System.out.println(b);
+
+        short s=sc.nextShort();
+        System.out.println(s);
+
+        long l=sc.nextLong();
+        System.out.println(l);
+    }
+}
