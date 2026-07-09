a565109458409a0196c74ac867bd5b1610c71399
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-09 14:32:49 +0530

    Learning the Syntax of if else in Java

---
+++ if_else.java
@@ -0,0 +1,14 @@
+public class if_else {
+    public static void main(String[] args) {
+        int age=17;
+        if (age>=18) {
+            System.out.println("He is an adult");
+        }
+        if (age>13 && age<18) {
+            System.err.println("He is a Teen");
+        }
+        else {
+            System.out.println("He is not adult");
+        }
+    }
+}
