b45dfdfe2b7afe2f576f72b24e05f75b1c5b7934
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-09 14:28:58 +0530

    Variables Practise

---
+++ Practise_variable_Question.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Practise_variable_Question {
+    public static void main(String agrs[]) {
+        Scanner sc=new Scanner(System.in);
+        // Question 1:-
+        /*int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        float avg=(a+b+c)/3;
+        System.out.println(avg);*/
+        
+        //Question 2:-
+        /*int side=sc.nextInt();
+        int area=side*side;
+        System.out.println("area="+area);
+        
+        //Question 3:-
+        float pencil=sc.nextFloat();
+        float pen=sc.nextFloat();
+        float eraser=sc.nextFloat();
+        float total_without_gst=pencil+pen+eraser;
+        System.out.println("Total Without GST="+total_without_gst);
+        float total_with_gst=(total_without_gst*0.18f)+total_without_gst;
+        System.out.println("Total With GST="+total_with_gst);*/
+        int $=44;
+        System.out.println($);
+    }
+}
