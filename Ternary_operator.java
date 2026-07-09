460f9e1fda020f9e4595d7fd3599cb69a84133a5
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-09 14:41:49 +0530

    Short Form of If Else Statement

---
+++ Ternary_operator.java
@@ -0,0 +1,8 @@
+public class Ternary_operator {
+    public static void main(String[] args) {
+        int larger=(5>3)?5:3;
+        String type=(5%2==0)?"even":"odd";
+        System.out.println(larger);
+        System.out.println(type);
+    }
+}
