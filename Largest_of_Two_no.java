5da2495e35eeaf6f5f35bab29f25f3665b21f0b8
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-09 14:34:14 +0530

    If else Practise Questions

---
+++ Largest_of_Two_no.java
@@ -0,0 +1,12 @@
+public class Largest_of_Two_no {
+    public static void main(String[] args) {
+        int a=1;
+        int b=3;
+        if (a>=b) {
+            System.out.println("'A' is the largest");
+        }
+        else {
+            System.out.println("'B' Is the Lergest");
+        }
+    }
+}
