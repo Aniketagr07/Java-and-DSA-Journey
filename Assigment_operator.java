b22936261a67805c5dadaecd5c8a17ed9734b3c6
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-09 14:30:54 +0530

    Understading about Operators

---
+++ Assigment_operator.java
@@ -0,0 +1,13 @@
+public class Assigment_operator {
+    public static void main(String args[]) {
+        int a=40;
+        a+=10;
+        System.out.println(a);
+        a-=10;
+        System.out.println(a);
+        a*=10;
+        System.out.println(a);
+        a/=10;
+        System.out.println(a);
+    }
+}
---
+++ Logical_Operators.java
@@ -0,0 +1,11 @@
+public class Logical_Operators {
+    public static void main(String[] args) {
+        System.out.println((3>2)&&(5>0));
+        System.out.println((3<2)&&(5>0));
+        System.out.println((3>2)||(5>0));
+        System.out.println((3<2)||(5>0));
+        System.out.println((3>0)||(5<0));
+        System.out.println((3<2)||(5<0));
+        System.out.println(!(3<2));
+    }
+}
