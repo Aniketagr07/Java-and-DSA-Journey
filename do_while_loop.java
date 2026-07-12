353431428dd37bc26beb37e987a1c51e15b1ff14
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-12 14:06:30 +0530

    Learning Do While loop Syntax

---
+++ do_while_loop.java
@@ -0,0 +1,9 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+        int c=1;
+        do{
+            System.out.println(c+" Hello World");
+            c++;
+        } while(c<=10);
+    }
+}
