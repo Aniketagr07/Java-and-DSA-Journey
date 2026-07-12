d30f6413757daa15e1b79b67d2eace1b8d8b64ad
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-12 13:58:23 +0530

    Learning While loop Syntax

---
+++ While_loop.java
@@ -0,0 +1,10 @@
+public class While_loop {
+    public static void main(String[] args) {
+        int a=1;
+        while (a<=100) {
+            System.out.print(a);
+            System.out.println("Hello world");
+            a++;
+        }
+    }
+}
