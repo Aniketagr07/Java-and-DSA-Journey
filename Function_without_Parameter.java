cee0605b5fba5ee4e5b3f359f54a8c8d64d75f30
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-16 01:06:12 +0530

    Learning the Syntax for creating a Function without Parameters

---
+++ Function_without_Parameter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Function_without_Parameter {
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return;
+    }
+    public static void main(String[] args) {
+         printHelloWorld();
+    }
+}
